bdf8c283b5991f08fc14bc19de0f0eeb75c74f7b
Sophia Hudson <sophialhudson@gmail.com>
2016-02-21 11:38:15 -0700

    Initial stubs of binary searcher test suite methods.

---
+++ java/src/test/java/io/sophialhudson/ads/arrays/TestBinarySearcher.java
@@ -0,0 +1,55 @@
+package io.sophialhudson.ads.arrays;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestBinarySearcher {
+
+  @Ignore
+  public void testByteArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testShortArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testIntArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testLongArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testFloatArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testDoubleArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testBooleanArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testCharArrayIndexOf() {
+
+  }
+
+  @Ignore
+  public void testObjectArrayIndexOf() {
+
+  }
+
+}
