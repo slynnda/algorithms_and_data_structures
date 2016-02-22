bf21e7fa1847d82f33cb74b3e1de24852b93676e
Sophia Hudson <sophialhudson@gmail.com>
2016-02-21 18:55:32 -0700

    Implementation stubs for static quicksort api

---
+++ java/src/main/java/io/sophialhudson/ads/arrays/QuickSort.java
@@ -0,0 +1,357 @@
+package io.sophialhudson.ads.arrays;
+
+/**
+ * This class provides implementations of the QuickSort algorithm that
+ * work on any primitive type and any class that subclasses Comparable.
+ *
+ * NOTE: These implementations utilize the Hoare partition scheme.
+ * NOTE: These implementations do in-place quicksort.
+ * 
+ * @author Sophia Hudson sophialhudson@gmail.com
+ */
+public class QuickSort {
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(byte[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(byte[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(byte[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(short[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(short[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(short[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(int[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(int[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private  static int partition(int[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(long[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(long[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+
+  private static int partition(long[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(float[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(float[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(float[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(double[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(double[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(double[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(boolean[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(boolean[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(boolean[] arr,  int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(char[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(char[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(char[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort the entire array and throw an exception if the
+   * array is null.
+   * 
+   * @param arr The array to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(Comparable[] arr) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place sort of the provided array. This method
+   * will sort only sort the segment of the array specified
+   * by the low and high arguments.
+   *
+   * @param arr The array to sort.
+   * @param low Lower bound of the segment to sort.
+   * @param high Upper bound of the segment to sort.
+   * @throws NullPointerException Attempted to sort a null array.
+   */
+  public static void sort(Comparable[] arr, int low, int high) throws NullPointerException, UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+  /**
+   * Do an in-place Hoare partitioning of the array segment specified
+   * by the low and high arguments. 
+   *
+   * @param arr The array to partition.
+   * @param low Lower bound of the segment to partition.
+   * @param high Upper bound of the segment to sort.
+   */
+  private static int partition(Comparable[] arr, int low, int high) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("This method has yet to be implemented.");
+  }
+
+}
